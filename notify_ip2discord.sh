209f62d4b234270db0e20bbdfd32e179247d7ab2
Adriano-lcp <adriano@hatsu.com.br>
2022-11-14 10:09:33 -0300

    corrigindo path do projeto

--- notify_ip2discord.sh
+++ notify_ip2discord.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 IP="$(curl ifconfig.me/ip)"
-LAST_IP="$(tail -n 1 $HOME/linux-server-ip-notification/last_ip.txt)"
-rm $HOME/linux-server-ip-notification/last_ip.txt
-echo $IP >> $HOME/linux-server-ip-notification/last_ip.txt
+LAST_IP="$(tail -n 1 $HOME/adriano-server-ip-notification/last_ip.txt)"
+rm $HOME/adriano-server-ip-notification/last_ip.txt
+echo $IP >> $HOME/adriano-server-ip-notification/last_ip.txt
 
 if [ "$IP" != "$LAST_IP" ]; then
-    $HOME/linux-server-ip-notification/discord.sh-1.6.1/discord.sh 
+    $HOME/adriano-server-ip-notification/discord.sh-1.6.1/discord.sh 
     --webhook-url=$WEBHOOK 
     --text "Novo IP do server: $IP"
 fi
